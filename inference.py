20ffadd901a3e3f03406028152f17e058c8b6828
mengfeil <mengfei.li@intel.com>
2023-02-13 00:20:11 -0800

    add oob scripts

---
+++ inference.py
@@ -0,0 +1,177 @@
+
+import os
+import torch
+import time
+import argparse
+import sys
+from PIL import Image
+from diffusers import StableDiffusionPipeline, DDPMPipeline
+
+MODEL_CLASSES = {
+    "stable-diffusion": (StableDiffusionPipeline),
+    "ddpm": (DDPMPipeline),
+}
+
+
+def image_grid(imgs, rows, cols):
+    assert len(imgs) == rows*cols
+
+    w, h = imgs[0].size
+    grid = Image.new('RGB', size=(cols*w, rows*h))
+    grid_w, grid_h = grid.size
+    
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i%cols*w, i//cols*h))
+    return grid
+
+def test(args, model, prompt):
+    total_sample = 0
+    total_time = 0.0
+    if args.profile:
+        if torch.cuda.is_available():
+            prof_act = [torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA]
+        else:
+            prof_act = [torch.profiler.ProfilerActivity.CPU]
+        with torch.profiler.profile(
+            activities=prof_act,
+            record_shapes=True,
+            schedule=torch.profiler.schedule(
+                wait=int(args.num_iter/2),
+                warmup=2,
+                active=1,
+            ),
+            on_trace_ready=trace_handler,
+        ) as p:
+            for i in range(args.num_iter):
+                elapsed = time.time()
+                if args.model_type == 'stable-diffusion':
+                    image = model(prompt, num_inference_steps=args.num_inference_steps).images[0]
+                else:
+                    image = model(batch_size=args.per_device_eval_batch_size).images[0]
+                if torch.cuda.is_available(): torch.cuda.synchronize()
+                p.step()
+                elapsed = time.time() - elapsed
+                print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+                if i >= args.num_warmup:
+                    total_time += elapsed
+                    total_sample += 1
+    else:
+        for i in range(args.num_iter):
+            elapsed = time.time()
+            if args.model_type == 'stable-diffusion':
+                image = model(prompt, num_inference_steps=args.num_inference_steps).images[0]
+            else:
+                image = model(batch_size=args.per_device_eval_batch_size).images[0]
+            if torch.cuda.is_available(): torch.cuda.synchronize()
+            elapsed = time.time() - elapsed
+            print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+            if i >= args.num_warmup:
+                total_time += elapsed
+                total_sample += 1
+
+    print("\n", "-"*20, "Summary", "-"*20)
+    latency = total_time / total_sample * 1000
+    throughput = total_sample / total_time
+    print("inference Latency: {} ms".format(latency))
+    print("inference Throughput: {} samples/s".format(throughput))
+
+    # save image
+    if args.save_image:
+        if args.model_name_or_path != 'stable-diffusion':
+            image.save(f"generated_image.png")
+        else:
+            from math import ceil
+            grid = image_grid(image, rows=args.image_rows, cols=ceil(args.batch_size / args.image_rows))
+            grid.save("astronaut_rides_horse-" + args.precision + ".png")
+
+def trace_handler(p):
+    output = p.key_averages().table(sort_by="self_cpu_time_total")
+    print(output)
+    import pathlib
+    timeline_dir = str(pathlib.Path.cwd()) + '/timeline/'
+    if not os.path.exists(timeline_dir):
+        try:
+            os.makedirs(timeline_dir)
+        except:
+            pass
+    timeline_file = timeline_dir + 'timeline-' + str(torch.backends.quantized.engine) + '-' + \
+                'sd-' + str(p.step_num) + '-' + str(os.getpid()) + '.json'
+    p.export_chrome_trace(timeline_file)
+
+
+if __name__ == '__main__':
+    #
+    parser = argparse.ArgumentParser(description='PyTorch Inference')
+    parser.add_argument("--model_name_or_path", type=str, default='CompVis/stable-diffusion-v1-4', help="model name")
+    parser.add_argument("--model_type", type=str, default='', help="model_type")
+    parser.add_argument('--prompt', default=["a photo of an astronaut riding a horse on mars"], type=list, help='prompt')
+    parser.add_argument('--device', default="cpu", type=str, help='cpu, cuda or xpu')
+    parser.add_argument('--precision', default="float32", type=str, help='precision')
+    parser.add_argument('--channels_last', default=1, type=int, help='Use NHWC or not')
+    parser.add_argument('--profile', action='store_true', default=False, help='collect timeline')
+    parser.add_argument('--num_iter', default=1, type=int, help='test iterations')
+    parser.add_argument('--num_warmup', default=0, type=int, help='test warmup')
+    parser.add_argument('--per_device_eval_batch_size', default=1, type=int, help='per_device_eval_batch_size')
+    parser.add_argument('--num_inference_steps', default=50, type=int, help='num_inference_steps')
+    parser.add_argument('--save_image', action='store_true', default=False, help='save image')
+    parser.add_argument('--image_rows', default=1, type=int, help='saved image array')
+    #
+    parser.add_argument('--ipex', action='store_true', default=False, help='enable ipex')
+    parser.add_argument('--jit', action='store_true', default=False, help='enable JIT')
+    parser.add_argument('--dpm_solver', action='store_true', default=False, help='enable DPM solver')
+    #
+    parser.add_argument('--do_eval', action='store_true', default=False, help='useless')
+    parser.add_argument('--overwrite_output_dir', action='store_true', default=False, help='useless')
+    parser.add_argument('--output_dir', default='', type=str, help='useless')
+    args = parser.parse_args()
+    print(args)
+
+    # Intialize
+    model_class = MODEL_CLASSES[args.model_type]
+    if args.dpm_solver:
+        from diffusers import DPMSolverMultistepScheduler
+        dpm = DPMSolverMultistepScheduler.from_pretrained(args.model_name_or_path, subfolder="scheduler")
+        model = model_class.from_pretrained(args.model_name_or_path, scheduler=dpm, torch_dtype=torch.float)
+        args.num_inference_steps = 20
+        print("---- Use DPM solver.")
+    else:
+        model = model_class.from_pretrained(args.model_name_or_path)
+    prompt = args.prompt * args.per_device_eval_batch_size
+    if args.device == 'cuda':
+        model = model.to("cuda")
+    if args.channels_last:
+        # model = model.to(memory_format=torch.channels_last)
+        model.unet = model.unet.to(memory_format=torch.channels_last)
+        if args.model_type == 'stable-diffusion':
+            model.vae = model.vae.to(memory_format=torch.channels_last)
+            model.text_encoder = model.text_encoder.to(memory_format=torch.channels_last)
+            model.safety_checker = model.safety_checker.to(memory_format=torch.channels_last)
+        print("---- Use NHWC model.")
+    if args.ipex:
+        import intel_extension_for_pytorch as ipex
+        print("---- Use IPEX")
+        if args.precision == "bfloat16":
+            model.unet = ipex.optimize(model.unet.eval(), dtype=torch.bfloat16, inplace=True)
+            if args.model_type == 'stable-diffusion':
+                model.vae = ipex.optimize(model.vae.eval(), dtype=torch.bfloat16, inplace=True)
+                model.text_encoder = ipex.optimize(model.text_encoder.eval(), dtype=torch.bfloat16, inplace=True)
+                model.safety_checker = ipex.optimize(model.safety_checker.eval(), dtype=torch.bfloat16, inplace=True)
+        else:
+            model.unet = ipex.optimize(model.unet.eval(), dtype=torch.float32, inplace=True)
+            if args.model_type == 'stable-diffusion':
+                model.vae = ipex.optimize(model.vae.eval(), dtype=torch.float32, inplace=True)
+                model.text_encoder = ipex.optimize(model.text_encoder.eval(), dtype=torch.float32, inplace=True)
+                model.safety_checker = ipex.optimize(model.safety_checker.eval(), dtype=torch.float32, inplace=True)
+
+    # start test
+    if args.precision == "bfloat16":
+        print("---- Use AMP bfloat16")
+        with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
+            test(args, model, prompt)
+    elif args.precision == "float16":
+        print("---- Use AMP float16")
+        with torch.cuda.amp.autocast(enabled=True, dtype=torch.float16):
+            test(args, model, prompt)
+    else:
+        test(args, model, prompt)
+
---
+++ launch_benchmark.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+set -xe
+
+function main {
+    # set common info
+    source oob-common/common.sh
+    init_params $@
+    fetch_device_info
+    set_environment
+
+    # requirements
+    pip uninstall -y transformers tokenizers
+    pip install -U transformers ftfy
+    python setup.py install
+
+    # if multiple use 'xxx,xxx,xxx'
+    model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
+    batch_size_list=($(echo "${batch_size}" |sed 's/,/ /g'))
+
+    # generate benchmark
+    for model_name in ${model_name_list[@]}
+    do
+        if [ "${model_name}" == "jmamou-ddpm-ema-flowers-64-distil-2layers" ];then
+            model_arch=' --model_name_or_path jmamou/ddpm-ema-flowers-64-distil-2layers --model_type ddpm '
+            num_inference_steps=1000
+        elif [ "${model_name}" == "jmamou-ddpm-ema-flowers-64-distil-4layers" ];then
+            model_arch=' --model_name_or_path jmamou/ddpm-ema-flowers-64-distil-4layers --model_type ddpm '
+            num_inference_steps=1000
+        elif [ "${model_name}" == "anton-l-ddpm-ema-flowers-64" ];then
+            model_arch=' --model_name_or_path anton-l/ddpm-ema-flowers-64 --model_type ddpm '
+            num_inference_steps=1000
+        elif [ "${model_name}" == "CompVis-stable-diffusion-v1-4" ];then
+            model_arch=' --model_name_or_path CompVis/stable-diffusion-v1-4 --model_type stable-diffusion '
+            num_inference_steps=50
+        elif [ "${model_name}" == "runwayml-stable-diffusion-v1-5" ];then
+            model_arch=' --model_name_or_path runwayml/stable-diffusion-v1-5 --model_type stable-diffusion '
+            num_inference_steps=50
+        else
+            echo "[Error] Not enable such model: $model_name"
+            exit 1
+        fi
+        # cache
+        python inference.py --device ${device} \
+            ${model_arch} --num_inference_steps 1 \
+            --channels_last ${channels_last} \
+            --num_iter 3 --num_warmup 1 \
+            --precision=${precision} \
+            --per_device_eval_batch_size ${batch_size} \
+            ${addtion_options}
+        #
+        for batch_size in ${batch_size_list[@]}
+        do
+            # clean workspace
+            logs_path_clean
+            # generate launch script for multiple instance
+            if [ "${OOB_USE_LAUNCHER}" == "1" ] && [ "${device}" != "cuda" ];then
+                generate_core_launcher
+            else
+                generate_core
+            fi
+            # launch
+            echo -e "\n\n\n\n Running..."
+            cat ${excute_cmd_file} |column -t > ${excute_cmd_file}.tmp
+            mv ${excute_cmd_file}.tmp ${excute_cmd_file}
+            source ${excute_cmd_file}
+            echo -e "Finished.\n\n\n\n"
+            # collect launch result
+            collect_perf_logs
+        done
+    done
+}
+
+# run
+function generate_core {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        # instances
+        if [ "${device}" != "cuda" ];then
+            OOB_EXEC_HEADER=" numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') "
+            OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
+        else
+            OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
+        fi
+        printf " ${OOB_EXEC_HEADER} \
+            python inference.py --device ${device} \
+                ${model_arch} --num_inference_steps $num_inference_steps \
+                --channels_last ${channels_last} \
+                --num_iter 3 --num_warmup 1 \
+                --precision=${precision} \
+                --per_device_eval_batch_size ${batch_size} \
+                ${addtion_options} \
+        > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        if [ "${numa_nodes_use}" == "0" ];then
+            break
+        fi
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+function generate_core_launcher {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        printf "python -m oob-common.launch --enable_jemalloc \
+                    --core_list $(echo ${device_array[@]} |sed 's/;.//g') \
+                    --log_file_prefix rcpi${real_cores_per_instance} \
+                    --log_path ${log_dir} \
+                    --ninstances ${#device_array[@]} \
+                    --ncore_per_instance ${real_cores_per_instance} \
+            inference.py --device ${device} \
+                ${model_arch} --num_inference_steps $num_inference_steps \
+                --channels_last ${channels_last} \
+                --num_iter 3 --num_warmup 1 \
+                --precision=${precision} \
+                --per_device_eval_batch_size ${batch_size} \
+                ${addtion_options} \
+        > /dev/null 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        break
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+# download common files
+rm -rf oob-common && git clone https://github.com/intel-sandbox/oob-common.git
+
+# Start
+main "$@"
